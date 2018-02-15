9fda638297f2f3eb5c951b20ab9eb474e7c57373
Jack Tomlinson <jack.t@mmtdigital.co.uk>
2018-02-15 08:47:58 +0000

    JT - Added support for multiple classnames.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import styles from './styles.scss';
-// import classnames from 'classnames';
+import classnames from 'classnames';
 
 class Button extends Component  {
-  constructor(props){
-    super(props);
-  }
-  render(){
 
-    // const classNames = classnames({ [styles.BtnStyle]: true }, { [styles.Operation]: false }, { [styles.BigButton]: false });
+    constructor() {
+        super();
+    }
 
-    return (
-      <button onClick={() => this.props.clickMe(this.props.value) } className={ styles.BtnStyle }>{ this.props.value }</button>
-    )
-  }
+    render(){
+
+        const classNames = classnames({ 
+          [styles.BtnStyle]: true , 
+          [styles.Operation]: false, // instead of false here you can drive styles from props if you like.
+          [styles.BigButton]: false 
+        });
+
+        return (
+            <button onClick={() => this.props.clickMe(this.props.value) } className={classNames}>{ this.props.value }</button>
+        )
+    }
 }
 
 export default Button;
