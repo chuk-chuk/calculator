274271fe3c4a4a04bd45e5d9c2a15756542b4a5e
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-03-14 16:43:31 +0000

    ad tests to header and a button components

--- src/components/Button/spec.js
+++ src/components/Button/spec.js
@@ -3,9 +3,29 @@ import Button from '../Button';
 
 import { shallow } from 'enzyme';
 
-xdescribe('<Button />', () => {
-  it('renders the button', () => {
-    const wrapper = shallow(<Button />);
-    expect(wrapper.contains(<button></button>)).toBe(true);
-  });
+describe('<Button />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        const onClick = jest.fn();
+        wrapper = shallow(<Button onClick={onClick} />);
+    });
+
+    it('renders the button', () => {
+
+        expect(wrapper.find('button').length).toBe(1);
+
+    });
+
+    it('renders a button value', () => {
+
+        const onClick = jest.fn();
+        wrapper = shallow(<Button onClick={onClick} />);
+
+        wrapper.prop('onClick')();
+
+        expect(wrapper.prop('onClick')).toBeCalled();
+
+    });
+
 });
--- src/components/Header/spec.js
+++ src/components/Header/spec.js
@@ -8,4 +8,16 @@ describe('<Header />', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find('h3').length).toBe(1);
   });
+
+  it('renders one logo image', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img').length).toBe(1)
+  });
+
+  it('renders one header element', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('header').length).toBe(1)
+  });
+
+
 });
