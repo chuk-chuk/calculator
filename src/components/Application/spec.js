d2952a2e9aee3994c8a8424b41c586e4ee385bd0
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-03-23 18:25:57 +0000

    fix tests for application component due to redux being added to manage the store

--- src/components/Application/spec.js
+++ src/components/Application/spec.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from '../Header';
-import Footer from '../Footer';
-import InputField from '../InputField';
-import Application from '../Application';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from '../Header'
+import Footer from '../Footer'
+import InputField from '../InputField'
+import Application from '../Application'
 
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
+import { mount, shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 describe('<Application />', () => {
+  const initialState = { clickedButton: '', resultBox: ''}
+  const mockStore = configureStore()
+  let wrapper, store
+// rewirte this test with provider and create the store
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Application />, div);
-  });
+  beforeEach(() => {
+    store = mockStore(initialState)
+    wrapper = mount(<Provider store={store}><Application/></Provider>);
+  })
+
+  it('prop clickedButton matches with initialState', () => {
+      expect(wrapper.find(Application).prop('clickedButton')).toEqual(initialState.input)
+   });
+
+  it('prop resultBox matches with initialState', () => {
+      expect(wrapper.find(Application).prop('resultBox')).toEqual(initialState.result)
+   });
 
   it('renders one <Header /> component', () => {
-    const wrapper = shallow(<Application />);
-    expect(wrapper.find(Header).length).toBe(1);
+    expect(wrapper.find(Application).length).toBe(1);
   });
 
   it('renders one <Footer /> component', () => {
-    const wrapper = shallow(<Application />);
     expect(wrapper.find(Footer).length).toBe(1);
   });
 
+  it('renders one <Footer /> component', () => {
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+
   it('renders two <InputField /> components', () =>{
-    const wrapper = shallow(<Application />);
     expect(wrapper.find(InputField).length).toBe(2);
   })
 });
